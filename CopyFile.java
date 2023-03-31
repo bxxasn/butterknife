980e5311fdf987d26c0c0cf8aa859d8b23907b7a
bxxasn <bxxasn@126.com>
2023-03-31 20:52:26 +0800

    Update CopyFile.java

--- CopyFile.java
+++ CopyFile.java
@@ -1,211 +1,106 @@
-package remove_butterknife;
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+文章标题：如何挑选一款好狗粮？狗粮什么牌子好？
+在养狗的时候，相信不少宠主都很苦恼，到底给狗狗吃什么狗粮比较合适吧，下面就让小编来教大家，如何挑选一款好狗粮！
 
-/**
- *在新的文件目录下，去掉Buterknife的R2和BindView
- */
-public class CopyFile {
+一、根据年龄阶段挑选狗粮
+狗粮一般分为奶糕、幼犬粮、成犬粮、老年粮四种，不同年龄的狗狗的吸收能力和营养需求存在差别，都喂一种狗粮的话，狗狗可能会营养不良或营养过剩。
+奶糕：适合断奶~3个月的幼犬吃
+注：这个国内商家做出来的噱头，不必要的产品，可以直接选幼犬粮。
+幼犬粮：适合断奶~3个月的幼犬吃
+成犬粮：适合8个月以上的狗狗吃
+注：小型犬发情期早，8~10个月可以吃成犬粮，中大型发情期迟，10个月~1岁吃成犬粮。
 
-    /**
-     * 复制目录
-     * @param fromDir
-     * @param toDir
-     * @throws IOException
-     */
-    public static void copyDir(String fromDir,String toDir) throws IOException{
-        //创建目录的File对象
-        File dirSouce = new File(fromDir);
-        //判断源目录是不是一个目录
-        if (!dirSouce.isDirectory()) {
-            //如果不是目录那就不复制
-            return;
-        }
-        //创建目标目录的File对象
-        File destDir = new File(toDir);
-        //如果目的目录不存在
-        if(!destDir.exists()){
-            //创建目的目录
-            destDir.mkdir();
-        }
-        //获取源目录下的File对象列表
-        File[]files = dirSouce.listFiles();
-        for (File file : files) {
-            //拼接新的fromDir(fromFile)和toDir(toFile)的路径
-            String strFrom = fromDir + File.separator + file.getName();
-            String strTo = toDir + File.separator + file.getName();
-            //判断File对象是目录还是文件
-            //判断是否是目录
-            if (file.isDirectory()) {
-                //递归调用复制目录的方法
-                copyDir(strFrom,strTo);
-            }
-            //判断是否是文件
-            if (file.isFile()) {
-                //递归调用复制文件的方法
-                copyFile(strFrom,strTo);
-            }
-        }
-    }
-    /**
-     * 复制文件
-     * @param fromFile
-     * @param toFile
-     * @throws IOException
-     */
-    public static void copyFile(String fromFile,String toFile) throws IOException {
-        
-        
-            boolean isFragment = false;
+二、无谷粮、商业粮、天然粮
+市场上的狗粮总共有3种分类：无谷粮、天然粮、商品粮，那么给狗狗选什么类型的狗粮比较合适呢？下面小编给大家分析一下。
+1、无谷粮
+无谷粮的特点就如字面意思一样，就是狗粮里面没有任何的谷物成分，是用其他碳水植物、果蔬，如豆类、薯类代替小麦等高麸质谷物。
+吃无谷粮的好处：
+（1）减少某些狗狗可能对谷物过敏的风险，而且更易吸收
+（2）可以很好地避免狗狗在饭后突然飙升的血糖水平，避免狗狗肥胖
+（3）不易引起肠胃问题
+2、天然粮
+天然狗粮是一款没有添加防腐物质，抗生素、激素、合成色素、合成诱导剂等，天然狗粮材料均来自于大自然，营养更加丰富，吸收率也更高。
+吃天然粮的好处：
+（1）够起到提高狗狗免疫力。
+（2）增强体质，延长寿命。
+（3）绝对安全、营养。
+3、商品粮
+商品粮主要由4D成份（Dead已死、Diseased有病、Dying垂死和Disabled残废）及其副产品（内脏、皮毛等）制成的狗粮。
+吃商品粮的好处：
+（1）价格便宜（材料不明、谷物的成分多）
+（2）口感好（添加诱食剂）
+总结：不考虑价格，品质来说，选择顺序是无谷粮＞天然粮＞商品粮
+三、如何挑选一款好狗粮？
+1、看配料表
+根据国家的标准，每一种食品的成分表都要按照重量进行排序，从含量最多的那一种开始。
+（1）第一位应该是肉
+狗粮是肉类与植物混合但以肉类为主，如果肉类注明是鸡肉、牛肉或是鱼肉等等，那说明这样的狗粮是比较好的狗粮。
+有些商家为了隐藏狗粮的缺陷，直接就写着禽类、肉，根本不知道是哪种肉！
+（2）原料有标出占比
+狗粮的配料表，最好是有原料占比的，对于公开占比的狗粮，证明对产品有信心，另外就是表明愿意接受监督，大部分都是好的狗粮。
+2、看成分表
+我们选择一款狗粮，首先要保证它的营养是充足、均衡的，尤其是出自于大厂的狗粮，肯定是符合了国家标准。
+先给大家看看一下国标GB/T 31216-2014参数：
+（1）粗蛋白
+国产粮有国标，里面的标准都是最低的，最差的狗粮也需要满足里面的要求，对于成犬≥18%，幼犬≥22%。
+猫咪对蛋白质的要求没有猫咪需求那么高，但是狗狗摄入蛋白质过少，会影响成长发育，狗狗摄入蛋白质过多，会让肝脏和肾脏都带来巨大的负担，引起肝脏疾病，肾衰竭。
+所以给狗狗选狗粮的时候，蛋白质含量一般来说，在22%-35%为宜。
+（2）粗脂肪
+狗粮里面的“粗脂肪”俗称“含油量”，适当的脂肪可以帮狗狗护肤美毛，促进脂溶性维生素ADE的吸收，但不能过量。
+国标粗脂肪含量成犬≥5.0%，幼犬≥8.0%。
+一般选择中脂肪的狗粮就可以，正常范围在13%~18%即可，脂肪太高狗狗吃了容易有脂肪肝，胰腺炎，便便软，肥胖问题。
+（3）粗灰分
+粗灰分是狗粮样品在550-600℃高温炉中，将所有有机物质全部燃烧，氧化后剩余的无机物残渣，是目前工艺无法避免的一种成分。
+国家标准狗粮里面的粗灰分值≤10%。
+优质、安全的狗粮，粗灰分不会超过10%，置粗灰分这个指标，是因为要防止不良商家在狗粮中添加低廉、无营养的原料。
+（4）粗纤维
+粗纤维是植物细胞壁的主要组成成分，包括纤维素、半纤维素、木质素及角质等成分，狗狗是杂食动物，吃适量的含粗纤维食物是有好处的。
+粗纤维能吸水，可以增加狗狗的饱腹感。
+粗纤维能刺激肠道蠕动，帮助有便秘困扰的狗狗排便，让狗狗的消化系统更加顺畅。
+国家标准狗粮里面的粗纤维值≤9%。
+（5）水溶性氯化物
+水溶性氯化物，就是含盐量，狗狗每天需要摄入一定的盐分，但是不能过量摄入，不然容易引来泪痕、毛发粗糙等问题。
+国标水溶性氯化物含量成犬≥0.09%，幼犬≥0.45%。
+（6）钙磷比
+钙磷比大概为1：1～2：1，最佳比例为1.2：1。
+国标最低标准为：
+钙≥0.6%（成犬），钙≥1.0%（幼犬） 总磷≥0.5%（成犬），总磷≥0.8%（幼犬）
+3、看检测报告
+选择狗粮的时候，看配料表和成分表只能筛选出合格的猫粮，具体找出更优秀的狗粮，就应该让商家拿出狗粮检测报告，因为小品牌的品控能力弱，容易出现品质方面问题，例如出现营养成分不达标，黄曲霉检测超标等问题。
+所以这些小品牌一般不敢公示检测报告，应尽量选择一些信息透明度高，有检查报告的狗粮
+还有一些优质的品牌，狗粮使用的原料也会公开透明，让狗狗吃得更放心。
 
+四、狗粮什么牌子好？
+选择狗粮，有2个大方向，一个是进口粮，一个是国产粮。
+在几年前，一般都推荐进口粮，因为产品好，质量有保障，大品牌。
+但在近几年，不少进口狗粮，频繁出现质量问题，国产狗粮的品质越来越好，很多性价比高的品牌出现。
+年货狗粮储备选购，不能饿着家里汪星人，6点选购要点    
+现在很多人喜欢家里养个宠物，猫咪和狗狗是最多的，的确，他们是人类多年来很好的朋友，对于很多人来说，也是他们的家人。养宠物可以带来很多欢乐，也会让人更好的思考人生。
+看得见表面的欢乐，却未必看得到别人后面的艰辛。自从自己的家里也来了汪星人之后，也有很多深深地体会，别的不说，吃饭就是头疼的问题。
+究竟是让狗狗同自己一起吃饭，还是自己制作狗粮，或者购买商品狗粮，这个问题当初曾经也困扰了我很久，也网上到处去看别人的经验。现在过了十年时候，我的建议还是，选购商品狗粮作为主要的食物来源，是个合适的选择。同时自己进餐的时候有选择的喂食少量，增加狗狗的幸福感也是必要的，毕竟我们是一家人，是吧，这样才有家的感觉。
 
-        Map<String, String> map = new HashMap<>();
-        File file = new File(fromFile);
-        if (file.getName().endsWith(".java")) {
-
-            FileOutputStream fos=new FileOutputStream(new File(toFile));
-            OutputStreamWriter osw=new OutputStreamWriter(fos, "UTF-8");
-            BufferedWriter  bw=new BufferedWriter(osw);
-
-            BufferedReader br = new BufferedReader(new FileReader(file));// 构造一个BufferedReader类来读取文件
-            String s = null;
-            boolean isNext = false;
-
-            String tempKey = "";
-
-            while ((s = br.readLine()) != null) {// 使用readLine方法，一次读一行
-                // view bindView的id
-                if (s.contains("@BindView")) {
-                    String str = s.substring(s.lastIndexOf(".") + 1, s.length() - 1);
-                    bw.write("//"+s+"\n");
-                    
-//                    System.out.println(str);
-                    tempKey = str;
-                    isNext = true;
-                    continue;
-                }
-
-                if (isNext) {
-                    //view 本地名称
-
-                    s = s.trim();   
-                    String []strs = s.split(" ");
-                    System.out.println(strs[0]);
-                    System.out.println(strs[1]);
-                    System.out.println("-----"+s+"-------");
-                     
-                    map.put(tempKey, strs[1].substring(0,strs[1].length()-1));
-                    //                        bw.write("        ");
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("        ");
-                    sb.append(strs[0]);
-                    sb.append(" ");
-                    sb.append(strs[1]);
-                    sb.append("\n");
-                    bw.write(sb.toString());
-                          
-                    isNext = false;
-                } else {
-                        if(s.trim().startsWith("public class")) {
-                            if(s.contains("fragment") || s.contains("Fragment")) {
-                                isFragment = true;
-                            }
-                             bw.write(s+"\n");
-                        }else if (s.contains("public") && s.contains("void") && s.contains("initView()") && s.contains("{")) {
-                        StringBuilder sb = new StringBuilder();
-                        sb.append(s);
-                        sb.append("\n");
-                        for (String key : map.keySet()) {
-                            //btn_xx = findViewById(R.id.xx)
-                            sb.append("        ");
-                            sb.append(map.get(key));
-                            sb.append(" = ");
-                            if(isFragment) {
-                                    sb.append("getView().");
-                            }
-                            sb.append("findViewById(R.id.");
-                            sb.append(key);
-                            sb.append(");");
-                            sb.append("\n");
-                        }
-//                        System.out.println(sb.toString());
-                        bw.write(sb.toString());
-                    } else if (s.contains("import") && s.contains(".R2")) {
-//                        import com.sskj.mine.R2;
-                        bw.write("//" + s + "\n");
-                    } else if (s.contains("butterknife")){
-//                        import butterknife.BindView;
-                        bw.write("//" + s + "\n");
-                    }else{
-                            bw.write(s+"\n");
-                        }
-                    }
-            }
-            br.close();
-            bw.close();
-            osw.close();
-            fos.close();
-        }else{
-            //字节输入流——读取文件
-            FileInputStream in = new FileInputStream(fromFile);
-            //字节输出流——写入文件
-            FileOutputStream out = new FileOutputStream(toFile);
-            //把读取到的内容写入新文件
-            //把字节数组设置大一些   1*1024*1024=1M
-            byte[] bs = new byte[1 * 1024 * 1024];
-            int count = 0;
-            while ((count = in.read(bs)) != -1) {
-                out.write(bs, 0, count);
-            }
-            //关闭流
-            in.close();
-            out.flush();
-            out.close();
-        }
-    }
-
-
-    // 使用示例
-//    public static void main(String[] args) {
-//
-//        String rootPath ="D:\\temp";
-//
-//        boolean isFile = true;
-//
-//        if(isFile){
-//            String resource =rootPath+"\\BindMobileActivity.java";
-//            String target = rootPath+"\\BindMobileActivity2.java";
-//
-//            File targetFile =new File(target);
-//            if(targetFile.exists()){
-//                targetFile.delete();
-//            }else{
-////            targetFile.mkdirs();
-//            }
-//
-//            try {
-//                copyFile(resource,target);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }else{
-//            String resource = rootPath+"\\dbex_android";
-//            String target =rootPath+"\\dex_copy";
-//
-//            File targetFile =new File(target);
-//            if(targetFile.exists()){
-//                targetFile.delete();
-//            }else{
-//                targetFile.mkdirs();
-//            }
-//            try {
-//                copyDir(resource, target);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//    }
-
-}
+选择商品狗粮的原因其实就那么几条，适应性广、营养均衡，有助于消化，方便喂食、可控制食量、可控制质量，也比较方便。自己做的话费时费力，而且你生病了没力气可以吃粥，那狗狗怎么办？让狗狗同自己一起吃，但人的食物对于狗狗来说偏咸，而且狗狗消化未必好，有一定的风险。
+用狗粮虽然方便，但选购狗粮却是个费神费力的事情，因为要考虑到质量，要考虑狗狗的适应性，还要考虑价格。个人认为下面几点是一点要考虑的。
+1. 价格
+便宜的一定不是好狗粮，贵的也不一定是好狗粮。商人永远是要牟利的，大概看看原材料成本，就知道大概了。其次，比同款便宜太多的狗粮一定有问题，不是假冒就是临期的，绝对不要买。
+2. 国产还是进口
+国产价格低，进口的价格高（主要是进口之后价格就高了，无奈），选哪个的话题一直很热门，其实要根据具体情况，有的是国外和进口卖的一样的，有的是进口的是中国专供的，至于国产，有的是国内公司，国内饲料，有的是国内公司，进口饲料。有的是国外公司品牌授权等等。
+个人的建议，还是根据荷包多少来决定吧。
+下面这个是我最近买的嘉蓝狗粮， 还附送了六包试吃装， 试吃装是小袋比较方便出行使用。 现在年货节还有不少促销活动， 也参与春节不打烊活动。
+3. 根据狗狗的需求来选择
+我这里说的需求就是狗狗各种需要了。首先的就是狗狗要爱吃，每个狗狗的兴趣和胃口不同，同样的好狗粮未必就适合你家的狗，换个就是了。其次就是根据狗狗的耐受度，肠胃是不是敏感。根据狗狗的体型，如果过胖的话，就要选择低脂的。
+4. 喂食量
+喂食量的多少同最终的伙食费多少密切相关，喂食量的多少也同你购买多大袋的狗粮有关，袋子越大单价越便宜，但是开封后保质期会缩短，味道会有变化，增加坏掉的风险。每个厂商的狗粮都会有不同体重的喂食量的参考值，可以来参考。
+5. 配料表
+成分表是很关键的，一般来说，厂商会在成分表上耍些小聪明，但不会搞大欺骗，毕竟那么多人民网友和友商盯着呢，是吧。所以，我们就是要看清成分表的一些大概指标，然后作为参考。为什么是大概指标，因为毕竟我们不是行家嘛，差不多就可以了，生活不能太精细了，会很累。
+那么要看哪些呢？几个小建议。
+a 肉类成分的多少
+狗狗是吃肉的，不是吃素的。吃素过多，狗狗会消化不好，大便软，臭，吃的还多。所以肉类必须要是主要的成分。至于是鸡肉还是鱼肉，这个要看狗狗的适应程度了。我自己一般选择鱼多一些的，毕竟鸡的生存环境要比鱼差一些。当然，主要还是看狗狗的适应性。
+b 添加剂
+为了给狗狗营养均衡，除了主原料，狗粮都会添加一些添加剂，例如维生素，这些没有什么问题，不过一些其他的，类似抗氧化剂，有的会采用了人工抗氧化剂BHA和BHT，某些国家对于这些是禁止添加的，据说会有引起重大疾病的风险。而我国的规范，似乎还没有对于宠物的规范，只有关于饲料的。而饲料是是允许一定量的。所以，如果采用了天然抗氧化剂的，例如迷迭香提取物的，相对于人工的应该会好一些。
+关于添加剂，个人的观点是能天然的当然最好，如果无法选用，用了人工的，只要含量别超标，也不用过于恐慌。
+c 其他
+其他就是把这些配料表的东西挨个百度下，做到心里有数。如果遇到配料表里最后有个“等”字的，建议心里要多个问号，自己斟酌下。选购狗粮不能光看名字是什么三文鱼之类的，还要看含量百分比。 国家宠物饲料管理规定对于配方声称首位占总量比例的要求， 要求其含量必须超过26%。配方声称的组合按照成分含量的降序排列的要求， 要求其含量必须超过3%。
+6 成分表
+这里就是狗粮里面的各种成分规划为化学的组合，例如蛋白质、脂肪、钙、水分、灰分等的占比多少，这个对于选择哪种狗粮，以及狗狗不适应狗粮后如何调整有参考作用。好犬粮的蛋白应该在30%以上，脂肪在15%左右。当然，过了这个标准后，蛋白也并不一定是越高越好，毕竟不同品种的狗狗对于高含量蛋白的适应性和吸收能力不同，还要根据实际情况选择。
+7 换粮过程
+这一点其实还是很重要的，因为不同品种的狗狗，同一品种不同个体的差异，不同阶段的差异还是存在的。像我家的狗狗原来脾胃还算比较强大，然而病过一次后就差了很多，调理了很长时间。建议选择5-7天食量的狗粮重量，这样万一不合适不至于浪费，同时有足够的狗粮可以完成转换过程。换粮要循序渐进，先少量添加，然后慢慢增量。从10%开始，逐步增加到100%，同时观察狗狗身体反应，狗便便的状态。注意换粮期间要生活有规律，从而可以更好的观察判断。
